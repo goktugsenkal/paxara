ff66f08e47c52924473b8a67662359cd53fb51a7
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-07-23 15:40:25 +0300

    Initialized 3 layer project

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            return Ok(product);
+        }
+    }
+}
