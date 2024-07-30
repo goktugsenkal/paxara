04d58bd656a6cb505d690dd8e149ae60c6e2fc65
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-07-30 14:39:01 +0300

    Fixed localStorage not added error.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { BasketService } from './basket/basket.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Paxara';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private basketService: BasketService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const basketId = localStorage.getItem('basket_id');
+      if (basketId) this.basketService.getBasket(basketId);
+    }
+  }
 }
