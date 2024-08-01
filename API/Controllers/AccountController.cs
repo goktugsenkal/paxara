ca36cb7f8540e611526f3d0e9fddb905c6e4418d
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-08-01 09:29:25 +0300

    Added email check to register endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -112,6 +112,16 @@ namespace API.Controllers
         [Route("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[]{
+                        "Email is already in use"
+                    }
+                });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
