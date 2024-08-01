8b713f72e728259f515d14d527416cbb049ee34a
Göktuğ Şenkal <goktugsenkal1881@gmail.com>
2024-08-01 15:47:21 +0300

    Feat(client): Update navbar buttons to be dynamic w.r.t. login status

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { BasketService } from '../../basket/basket.service';
 import { BasketItem } from '../../shared/models/basket';
+import { AccountService } from '../../account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,7 +10,7 @@ import { BasketItem } from '../../shared/models/basket';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-  constructor(public basketService: BasketService) {}
+  constructor(public basketService: BasketService, public accountService: AccountService) {}
 
   getCount(items: BasketItem[]){
     return items.reduce((sum, item) => sum + item.quantity, 0 )
